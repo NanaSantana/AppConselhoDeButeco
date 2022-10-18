be204866dd4ad152fe19cba3e2d3e9304cb01eb3
Usuario <Usuario@CPD02-M02>
2022-10-18 12:23:35 -0300

    Adicionar arquivos de projeto.

---
+++ AppConselhoDeButeco/AppConselhoDeButeco/Model/Mensagem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConselhoDeButeco.Model
+{
+    class Mensagem
+    {
+        public string Title { get; set; }
+        public string Id { get; set; }
+        public string Conselho { get; set; }
+
+        public Mensagem()
+        {
+            this.Title = " "; 
+            this.Id = " ";
+            this.Conselho = " ";
+        }
+    }
+}
---
+++ AppConselhoDeButeco/AppConselhoDeButeco/Services/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppConselhoDeButeco.Model;
+
+namespace AppConselhoDeButeco.Services
+{
+    class DataService
+    {
+        public static async Task<Mensagem> GetMensagem()
+        {
+            string appId = "2";
+
+            string queryString = "https://api.adviceslip.com/advice";
+            dynamic resultado = await getDataFromService(queryString).ConfigureAwait(false);
+
+            if (resultado[""] != null)
+            {
+                Mensagem mensagem = new Mensagem();
+
+                mensagem.Title = (string)resultado["slip"];
+                mensagem.Id = (string)resultado["id"];
+                mensagem.Conselho = (string)resultado["advice"];
+                return mensagem;
+            }
+        }
+    }
+}
